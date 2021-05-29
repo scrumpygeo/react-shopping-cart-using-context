ff4263b5f14a8a2434863791a55f892fffb7c9fa
scrumpygeo <scrumpygeo@yahoo.co.uk>
2021-05-29 19:27:27 +0100

    initial setup

---
+++ src/data/data.js
@@ -0,0 +1,88 @@
+const products = [
+  {
+    id: '1',
+    name: 'Bar at the Folies Bergère',
+    image:
+      'https://www.getty.edu/art/exhibitions/manet_bar/images/bar_detail.jpg',
+    description:
+      'Painting by Manet depicting a scene in the Folies Bergère nightclub in Paris. (1882).',
+    brand: 'Manet',
+    category: 'French Modernist',
+    price: 89.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    id: '2',
+    name: 'Asakusa Rice Field During the Cock Festival at Otori Shrine.',
+    image:
+      'https://kiamaartgallery.files.wordpress.com/2015/05/utagawa-hiroshige-asakusa-rice-field-during-the-cock-festival-at-otori-shrine-1857.png',
+    description:
+      'Ukiyo-e colour woodblock print by Japanese Artist Hokusai. (1857)',
+    brand: 'Hokusai',
+    category: 'Ukiyo-e',
+    price: 599.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    id: '3',
+    name: 'Jas de Bouffan, The Pool-Cezanne',
+    image:
+      'https://uploads7.wikiart.org/images/paul-cezanne/jas-de-bouffan-the-pool.jpg!Large.jpg',
+    description:
+      'Painting of the Jas de Bouffan was the estate on the outskirts of Aix-en-Provence by French Post-Impressionist artist Paul Cezanne (1857)',
+    brand: 'Cezanne',
+    category: 'Post Impressionist',
+    price: 929.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    id: '4',
+    name: 'Lady with an Ermine',
+    image:
+      'https://www.croxgallery.co.uk/media/catalog/product/cache/12/image/359x490/a4b764e5482f62fb51dae89ed3917009/l/a/lady-ermine-da-vinci.jpg',
+    description:
+      'One of only four paintings of women by renaissance artist Leonardo Da Vinci. (1489–1490)',
+    brand: 'Da Vinci',
+    category: 'Renaissance',
+    price: 399.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    id: '5',
+    name: 'The Great Wave Off Kanagawa',
+    image:
+      'https://johngaber.files.wordpress.com/2017/10/the-great-wave-off-kanagawa.jpg',
+    description:
+      'Colour woodblock print of a great wave threatening boats off the coast in the Sagami Bay by Japanese Artist Hokusai. (1829-1833)',
+    brand: 'Hokusai',
+    category: 'Ukiyo-e',
+    price: 49.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    id: '6',
+    name: 'The Luncheon of the Boating Party',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/1/19/Luncheon_of_the_Boating_Party_Renoir_1881.jpg',
+    description:
+      'Paintin by Renoir of a group of his friends relaxing on a balcony at the Maison Fournaise restaurant by the Seine. (1881)',
+    brand: 'Renoir',
+    category: 'Impressionist',
+    price: 29.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+]
+
+export default products
