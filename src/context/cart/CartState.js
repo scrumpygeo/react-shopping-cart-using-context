378ede5edba6fe9a031dcd988b1aa385ffe33461
scrumpygeo <scrumpygeo@yahoo.co.uk>
2021-05-31 13:55:44 +0100

    CartState

---
+++ src/context/cart/CartState.js
@@ -0,0 +1,46 @@
+import { useReducer } from 'react'
+import CartContext from './CartContext'
+import CartReducer from './CartReducer'
+import { SHOW_HIDE_CART, ADD_TO_CART, REMOVE_ITEM, CLEAR_CART } from '../types'
+
+const CartState = ({ children }) => {
+  const initialState = {
+    showCart: false,
+    cartItems: [],
+  }
+
+  const [state, dispatch] = useReducer(CartReducer, initialState)
+
+  const addToCart = (item) => {
+    dispatch({ type: ADD_TO_CART, payload: item })
+  }
+
+  const showHideCart = () => {
+    dispatch({ type: SHOW_HIDE_CART })
+  }
+
+  const removeItem = (id) => {
+    dispatch({ type: REMOVE_ITEM, payload: id })
+  }
+
+  const clearCart = () => {
+    dispatch({ type: CLEAR_CART })
+  }
+
+  return (
+    <CartContext.Provider
+      value={{
+        showCart: state.showCart,
+        cartItems: state.cartItems,
+        addToCart,
+        showHideCart,
+        removeItem,
+        clearCart,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  )
+}
+
+export default CartState
