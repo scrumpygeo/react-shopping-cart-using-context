f5b51197472a6748a570dc3b142381bf01000191
scrumpygeo <scrumpygeo@yahoo.co.uk>
2021-05-31 14:56:16 +0100

    add cart total to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import CartContext from '../context/cart/CartContext'
 
 const Navbar = () => {
+  const { cartItems, showHideCart } = useContext(CartContext)
+  const total = cartItems.reduce((a, c) => (a += c.qty), 0)
+
   return (
     <nav>
       <div className='nav_left'>Art Store</div>
       <div className='nav_right'>
         <div className='cart_icon'>
-          <i className='fa fa-shopping-cart'></i>
+          <i className='fa fa-shopping-cart' onclilck={showHideCart}></i>
+          {cartItems.length > 0 && (
+            <div className='item_count'>
+              <span>{total}</span>
+            </div>
+          )}
         </div>
       </div>
     </nav>
