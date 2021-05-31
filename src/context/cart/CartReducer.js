2bdbf6419d373c2f296b41a4cd3b5b4057164a57
scrumpygeo <scrumpygeo@yahoo.co.uk>
2021-05-31 14:33:30 +0100

    add Cart State

---
+++ src/context/cart/CartReducer.js
@@ -0,0 +1,65 @@
+import { SHOW_HIDE_CART, ADD_TO_CART, REMOVE_ITEM, CLEAR_CART } from '../types'
+
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case SHOW_HIDE_CART: {
+      return {
+        ...state,
+        showCart: !state.showCart,
+      }
+    }
+    case ADD_TO_CART: {
+      const item = action.payload
+      const updatedCart = [...state.cartItems]
+      const updatedItemIndex = updatedCart.findIndex((x) => x._id === item._id)
+      console.log(updatedItemIndex)
+
+      // if item not already in cart, add it and set qyt to  1
+      if (updatedItemIndex < 0) {
+        updatedCart.push({ ...item, qty: 1 })
+      } else {
+        const updatedItem = {
+          ...updatedCart[updatedItemIndex],
+        }
+        updatedItem.qty++
+        updatedCart[updatedItemIndex] = updatedItem
+      }
+
+      return { ...state, cartItems: updatedCart }
+    }
+    case REMOVE_ITEM: {
+      const item = action.payload
+
+      const updatedCart = [...state.cartItems]
+      const updatedItemIndex = updatedCart.findIndex((x) => x._id === item)
+      // console.log(updatedItemIndex)
+
+      const updatedItem = {
+        ...updatedCart[updatedItemIndex],
+      }
+
+      updatedItem.qty--
+
+      if (updatedItem.qty <= 0) {
+        updatedCart.splice(updatedItemIndex, 1)
+      } else {
+        updatedCart[updatedItemIndex] = updatedItem
+      }
+
+      return { ...state, cartItems: updatedCart }
+    }
+
+    case CLEAR_CART: {
+      return {
+        ...state,
+        cartItems: [],
+      }
+    }
+
+    default: {
+      throw new Error(`unknown action ${action.type}`)
+    }
+  }
+}
+
+export default cartReducer
